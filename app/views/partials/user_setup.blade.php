51a887733469f36eeaebf1d98a07b918a49489a2
Benny Cardenas <benitocardenas@gmail.com>
2015-05-13 10:55:45 -0500

    Side menu for user has been set up

--- app/views/partials/user_setup.blade.php
+++ app/views/partials/user_setup.blade.php
@@ -4,11 +4,12 @@
 
 <head>
     <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 
 <style type="text/css">
     #formwidth{
-        width: 100px;
+        width: 150px;
     }
 </style>
 
@@ -18,25 +19,11 @@
 
     <div class="pure-menu custom-restricted-width">
         <ul class="pure-menu-list">
-            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Account Settings</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link"><i class="fa fa-cog"></i> Settings</a></li>
             <li class="pure-menu-item"><a href="#" class="pure-menu-link">Create Party</a></li>
             <li class="pure-menu-item"><a href="#" class="pure-menu-link">Manage Party</a></li>
-            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Finance</a></li>
-            <li class="pure-menu-item pure-menu-has-children">
-                <a href="#" id="menuLink1" class="pure-menu-link">More</a>
-                <ul class="pure-menu-children">
-                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Autos</a></li>
-                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Flickr</a></li>
-                    <li class="pure-menu-item pure-menu-has-children">
-                        <a href="#" id="menuLink1" class="pure-menu-link">Even More</a>
-                        <ul class="pure-menu-children">
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Foo</a></li>
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Bar</a></li>
-                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Baz</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Confirmed Parties</a></li>
+            
         </ul>
     </div>
 
