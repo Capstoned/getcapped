f71abdc75f22ce6f0c8dd53a541183685a96fda8
Keyasha Brothern <kmbrothern85@gmail.com>
2015-05-20 16:21:54 -0500

    added inline styling to css file

--- app/views/parties/create.blade.php
+++ app/views/parties/create.blade.php
@@ -1,34 +1,12 @@
+@extends('layouts.master')
 
+@section('head')
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<link rel="stylesheet" href="/css/partyplanner.css">
 
-<style type="text/css">
-  .partytype{
-    float:left;
-  }
-  .service{
-    float: left;
-    padding-left: 40px;
-  }
-
-  .spacer {
-    padding-top: 300px;
-  }
-</style>
-
-
-
-{{-- 'party_type' => 'required',
-		'address' => 'required',
-		'city' => 'required',
-		'state' => 'required',
-		'zip_code' => 'required',
-		'event_date' => 'required'
-
-public static $partyTypes = ['0' => 'childBday', 
-						'1' => 'anniversary', 
-						'2' => 'adultBday', 
-						'3' => 'other']; --}}
-
-{{-- <div class ="spacer"></div> --}}
+@stop
+@section('content')
+<div class="container">
 
 {{Form::open(array('method' => 'POST', 'action' => 'PartiesController@store', 'class' => 'pure-form spacer form-box-two'))}}
 
@@ -37,8 +15,8 @@ public static $partyTypes = ['0' => 'childBday',
 
 
 
-
-{{ Form::label ('party_type', 'Select Your Party Type')}}
+{{-- Removed all labels from forms and made them h2's so they would be larger --}}
+{{-- {{ Form::label ('party_type', 'Select Your Party Type')}} --}}
 <h2> Select Your Party Type</h2>
 
 
@@ -52,7 +30,7 @@ public static $partyTypes = ['0' => 'childBday',
 <br>
 
 
-{{ Form::label ('service_code', 'Select Desired Services')}}
+{{-- {{ Form::label ('service_code', 'Select Desired Services')}} --}}
 <h2> Select Desired Services</h2>
 
 {{Form::checkbox('service_code', '0')}} Balloons
@@ -64,10 +42,10 @@ public static $partyTypes = ['0' => 'childBday',
 
 
 
-{{ Form::label ('address', 'Select Party Location')}}
+{{-- {{ Form::label ('address', 'Select Party Location')}} --}}
 <h2> Select Party Location</h2>
 
-{{ Form::label ('address', 'Address')}}
+{{ Form::label ('address', null, array('placeholder' => 'Address')) }}
 <br>
 {{Form::text('address', 'Address')}}
 <br>
@@ -92,11 +70,30 @@ public static $partyTypes = ['0' => 'childBday',
 {{Form::textarea('comments', null, ['size' => '30x5'])}}
 <br>
 
-{{ Form::label ('Submit', 'Submit')}}
+{{-- {{ Form::label ('Submit', 'Submit')}} --}}
 <br>
 {{ Form::submit('Submit', array('class' => 'btn')) }}
 
 {{Form::close()}}
+
+</div>
+@stop
+
+{{-- 'party_type' => 'required',
+		'address' => 'required',
+		'city' => 'required',
+		'state' => 'required',
+		'zip_code' => 'required',
+		'event_date' => 'required'
+
+public static $partyTypes = ['0' => 'childBday', 
+						'1' => 'anniversary', 
+						'2' => 'adultBday', 
+						'3' => 'other']; --}}
+
+{{-- <div class ="spacer"></div> --}}
+
+
 
 
 
