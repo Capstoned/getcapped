f456b9925f1c52634c1810f8c73f8a2f9d0d29e3
Keyasha Brothern <kmbrothern85@gmail.com>
2015-05-22 10:27:13 -0500

    Added divs for parties and moved calendar on dashboard

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -42,6 +42,51 @@
 
 <!-- change from img to background image for this div -->
     <section id="section-one">
+<div class="container">
+    <div class ="mainbox">
+
+
+
+  @foreach(Auth::user()->parties as $userParty)
+    
+
+    <div class="parties">
+    
+      <h4>Party Type</h4>
+      
+      {{Party::$partyTypes[$userParty->party_type] }}
+      <br>
+
+      <h4>Event Date</h4>
+      {{ $userParty->getEventDate($userParty->event_date) }}
+      <br>
+      
+      <h4>Services Requested</h4>
+        <?php $services = $userParty->services; ?>
+      @foreach($services as $service)
+        {{ $service->description }} <br>
+      @endforeach
+
+
+      <h4>Address</h4>
+      {{ $userParty->address }}
+      <br>
+      {{ $userParty->city }}
+      <br>
+      {{ $userParty->state }}
+      <br>
+      {{ $userParty->zip_code }}
+      
+      <h4>Comments</h4>
+      {{ $userParty->comments }}
+
+    </div>
+   
+
+  @endforeach
+
+  </div>
+</div>
 
         <div id="calendar">
         <div id="mini-clndr">
--- app/views/users/users-dash.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-
-
-{{-- @include('parties.create') --}}
-
-@include('parties.show')
-
-}
